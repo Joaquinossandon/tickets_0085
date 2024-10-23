dba4fbb986dd414e7bb2f2543371140fa389939d
Joaquinossandon <joaquin.ossandon@inacapmail.cl>
2024-10-23 09:53:58 -0300

    se agrega breve explicacion de path

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ hbs.registerPartials(__dirname + "/src/views/partials", (err) => {});
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "/src/views"));
 
+// el modulo <path> corrige este error
+// C:\users\joaqu\Desktop
+// __dirname + "/src/views/partials" => C:\users\joaqu\Desktop/src/views/partials
+// /ghost/home/desktop
+// __dirname + "/src/views/partials" => /user/home/proyecto/src/views/partials
+
+
 // midleware
 app.use(bodyParser.json());
 
